a653f9a1960e0a37fe94d0980c8a36c65c053668
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-04-21 15:56:09 +0100

    fix(deps): bump all to latest + pass plarform to bot settings

--- settings.go
+++ settings.go
@@ -7,7 +7,7 @@ import (
 
 // NewTelegramBot creates definition of new telegram bot
 func NewTelegramBot(mode strongo.Environment, profile, code, token, paymentTestToken, paymentToken, gaToken string, locale strongo.Locale) botsfw.BotSettings {
-	settings := botsfw.NewBotSettings(mode, profile, code, "", token, gaToken, locale)
+	settings := botsfw.NewBotSettings(botsfw.PlatformTelegram, mode, profile, code, "", token, gaToken, locale)
 	settings.PaymentTestToken = paymentTestToken
 	settings.PaymentToken = paymentToken
 	return settings
