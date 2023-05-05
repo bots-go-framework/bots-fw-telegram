a498439fa886ca321e49b6792fd05e9092620827
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-05-05 12:18:58 +0100

    fix(deps): latest bots-fw

--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.20
 
 require (
 	github.com/bots-go-framework/bots-api-telegram v0.2.2
-	github.com/bots-go-framework/bots-fw v0.11.0
+	github.com/bots-go-framework/bots-fw v0.12.0
 	github.com/bots-go-framework/bots-fw-dalgo v0.0.5
 	github.com/dal-go/dalgo v0.2.26
 	github.com/pquerna/ffjson v0.0.0-20190930134022-aa0246cd15f7
 	github.com/stretchr/testify v1.8.2
-	github.com/strongo/app v0.2.4
+	github.com/strongo/app v0.3.0
+	github.com/strongo/i18n v0.0.2
 	github.com/strongo/log v0.2.3
 )
 
--- settings.go
+++ settings.go
@@ -3,10 +3,11 @@ package telegram
 import (
 	"github.com/bots-go-framework/bots-fw/botsfw"
 	"github.com/strongo/app"
+	"github.com/strongo/i18n"
 )
 
 // NewTelegramBot creates definition of new telegram bot
-func NewTelegramBot(mode strongo.Environment, profile, code, token, paymentTestToken, paymentToken, gaToken string, locale strongo.Locale) botsfw.BotSettings {
+func NewTelegramBot(mode strongo.Environment, profile, code, token, paymentTestToken, paymentToken, gaToken string, locale i18n.Locale) botsfw.BotSettings {
 	settings := botsfw.NewBotSettings(botsfw.PlatformTelegram, mode, profile, code, "", token, gaToken, locale)
 	settings.PaymentTestToken = paymentTestToken
 	settings.PaymentToken = paymentToken
